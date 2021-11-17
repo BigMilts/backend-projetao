06b36f955eecb50f48276c6cd5ce14bebe6755e5
vlms <vlms@cin.ufpe.br>
2021-11-17 19:52:03 -0300

    Importação e expotação de módulos

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Profile } from './entities/profile.entity';
+import { InterestPointsModule } from 'src/interest-points/interest-points.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Profile])],
+  imports: [TypeOrmModule.forFeature([User, Profile]), InterestPointsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
