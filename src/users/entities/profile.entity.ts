dad8b442ca546961f343f2d8951894257a3390ab
vlms <vlms@cin.ufpe.br>
2021-11-27 14:34:06 -0300

    Documentação de profile

--- src/users/entities/profile.entity.ts
+++ src/users/entities/profile.entity.ts
@@ -1,14 +1,19 @@
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
 import { Itinerary } from 'src/interest-points/entities/itinerary.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
+@ApiTags('Profile')
 @Entity()
 export class Profile {
+  @ApiProperty({name: 'id'})
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  @ApiProperty({name: 'category'})
   @Column()
   category: string;
 
+  @ApiProperty({name: 'itineraries'})
   @OneToMany(() => Itinerary, (itinerary) => itinerary.profile)
   itineraries: Itinerary[];
 }
